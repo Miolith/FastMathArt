6a02a3a68021ccd7e6aa223c011c8e1fc1696e97
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2024-02-02 20:54:00 +0100

    save to video

--- fastmathart/render.cpp
+++ fastmathart/render.cpp
@@ -1,20 +1,53 @@
-#include "render.h"
-
 #include <iostream>
 #include <string_view>
+#include <memory>
+#include <cstdio>
+#include <fstream>
+#include <string>
+
+#include "render.h"
+
+void save_to_video_file(uint8_t framebuffer[], std::string_view filename, int fps, int width, int height, int frames)
+{
+    std::cout << "Saving to video file " << filename << std::endl;
+    std::cout << "Frame rate: " << fps << std::endl;
+    std::cout << "Frame count: " << frames << std::endl;
+    std::cout << "Frame width: " << width << std::endl;
+    std::cout << "Frame height: " << height << std::endl;
+
+    // Save the framebuffer to a video file
+    // execute ffmpeg
+    std::string command = "ffmpeg -y -f rawvideo -vcodec rawvideo -s "
+                + std::to_string(width)
+                + "x"
+                + std::to_string(height)
+                + " -pix_fmt rgb24 -r "
+                + std::to_string(fps) +
+                " -i - -an -vcodec mpeg4 -q:v 5 -f mp4 "
+                + std::string(filename);
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "w"), pclose);
+    if (!pipe)
+    {
+        std::cerr << "popen() failed!" << std::endl;
+        return;
+    }
+    fwrite(framebuffer, 1, width * height * frames * 3, pipe.get());
+}
 
 void render_element(Wait *elem, int width, int height)
 {
     std::cout << "Waiting for " << elem->seconds << " seconds" << std::endl;
+    int fps = 24;
+    int frames = elem->seconds * fps;
+    
+    auto pixel_list = std::make_unique<uint8_t[]>(width * height * frames * 3);
+
+    save_to_video_file(pixel_list.get(), "wait.mp4", fps, width, height, frames);
 }
 
 void cast_then_render_element(SceneElement *elem, int width, int height)
 {
-    if (elem == nullptr)
-    {
-        std::cout << "Null element" << std::endl;
-        return;
-    }
 
     if (elem->elem == nullptr)
     {
@@ -35,5 +68,12 @@ void cast_then_render_element(SceneElement *elem, int width, int height)
 void render_scene(SceneElement *elem, int width, int height,
                   std::string_view filename)
 {
+    if (elem == nullptr)
+    {
+        std::cout << "Null scene" << std::endl;
+        return;
+    }
+
+    std::cout << "Rendering scene to " << filename << std::endl;
     cast_then_render_element(elem, width, height);
 }
--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-import sys
 from setuptools import setup, Extension
-
-debug = '--debug' in sys.argv
-if debug:
-    sys.argv.remove('--debug')
 
 module = Extension(
     'fastmathart.cbindings',
     sources=['fastmathart/cbindings.cpp', 'fastmathart/render.cpp'],
     language='c++',
-    extra_compile_args=['-g', '-fno-optimize-sibling-calls', '-fsanitize=address'] if debug else ['-O3'],
-
+    extra_compile_args=['-std=c++2b'],
 )
 
 setup(
@@ -19,3 +13,4 @@ setup(
     description='Fast math art library',
     ext_modules=[module],
 )
+
