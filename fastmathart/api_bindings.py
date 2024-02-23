c3846568fab98f3e5d12c3d904d1dcfda8d61fdf
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2024-02-23 14:15:43 +0100

    Cmake config

--- fastmathart/api_bindings.py
+++ fastmathart/api_bindings.py
@@ -4,8 +4,8 @@ from fastmathart.scene import SceneElement
 from fastmathart.config import ConfigBinding
 import glob
 
-lib_file= glob.glob("build/lib*/fastmathart/cbindings*")
-lib = ctypeslib.load_library("cbindings", lib_file[0])
+lib_file= glob.glob("build/libfma*")
+lib = ctypeslib.load_library("libfma", lib_file[0])
 
 lib.render.argtypes = [POINTER(SceneElement), POINTER(ConfigBinding), c_char_p]
 lib.render.restype = None
