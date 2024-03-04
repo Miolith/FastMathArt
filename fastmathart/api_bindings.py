ff7431731ee2590f83395058d69eb1134d0cdffb
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2024-03-04 18:24:40 +0100

    Try better compatibility with windows

--- fastmathart/api_bindings.py
+++ fastmathart/api_bindings.py
@@ -1,11 +1,21 @@
-from ctypes import POINTER, c_char_p
-from numpy import ctypeslib
+from ctypes import POINTER, c_char_p, cdll
+import platform
 from fastmathart.scene import SceneElement
 from fastmathart.config import ConfigBinding
-import glob
+from glob import glob
 
-lib_file= glob.glob("build/libfma*")
-lib = ctypeslib.load_library("libfma", lib_file[0])
+files = []
+
+if platform.uname()[0] == "Windows":
+    files = glob("build/*fma*.dll")
+elif platform.uname()[0] == "Linux":
+    files = glob("build/*fma*.so")
+elif platform.uname()[0] == "Darwin":
+    files = glob("build/*fma*.dylib")
+else:
+    raise Exception("Unsupported OS")
+
+lib = cdll.LoadLibrary(files[0])
 
 lib.render.argtypes = [POINTER(SceneElement), POINTER(ConfigBinding), c_char_p]
 lib.render.restype = None
